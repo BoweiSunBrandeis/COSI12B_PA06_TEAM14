c52f53d4ca5707d344759afcce1f5cc234bb0c07
Gix <itsgix01@gmail.com>
2020-04-19 12:14:24 -0400

    Implemented file reader (part 4)

--- src/pa06/Cluster.java
+++ src/pa06/Cluster.java
@@ -7,45 +7,50 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * A cluster is a cluster point (which is itself a sample)
- * and a list of Samples (the one's closest to that sample point, ideally).
+ * A cluster is a cluster point (which is itself a sample) and a list of Samples
+ * (the one's closest to that sample point, ideally).
+ * 
  * @author Team 14
  *
  */
 public class Cluster {
 	private Sample clusterPoint;
 	private ArrayList<Sample> cluster;
-	
+
 	public Cluster(ArrayList<Sample> samples) {
 		this.cluster = samples;
-		
+
 	}
-	
+
 	/**
 	 * choose k items from the Sample list
+	 * 
 	 * @param the number of samples to pick
 	 * @return a list of elements which are randomly chosen
 	 */
 	public Sample[] randomPick(int num) {
 		Collections.shuffle(cluster);
 		Sample[] res = new Sample[num];
-		for(int i=0; i<num; i++) {
+		for (int i = 0; i < num; i++) {
 			res[i] = cluster.get(i);
 		}
 		return res;
 	}
-	
-	
+
+	public void add(Sample sample) {
+		this.cluster.add(sample);
+	}
+
 	public static void main(String[] args) {
-		double[] p1 = {1.1d, 2.1d, 3.14, 2.71};
-		double[] p2 = {1.1, 2.1, 3.14, 1.71};
+		double[] p1 = { 1.1d, 2.1d, 3.14, 2.71 };
+		double[] p2 = { 1.1, 2.1, 3.14, 1.71 };
 		Sample s1 = new Sample(p1);
 		Sample s2 = new Sample(p2);
-		
+
 		ArrayList<Sample> samples = new ArrayList<Sample>();
 		samples.add(s1);
 		samples.add(s2);
-		
+
 		Cluster cluster = new Cluster(samples);
 		System.out.println(cluster.randomPick(1)[0].toString());
 		System.out.println(cluster.randomPick(1)[0].toString());
--- src/pa06/KMeans.java
+++ src/pa06/KMeans.java
@@ -2,9 +2,12 @@ package pa06;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * Blue print for the K-Means class
+ * 
  * @author Team 14
  *
  */
@@ -12,12 +15,37 @@ import java.util.Scanner;
 public class KMeans {
 	ArrayList<Cluster>[] cluster;
 	Cluster originalData;
-	
+
 	public KMeans() {
-		
+
 	}
-	
-	
+
+	/**
+	 * reads a file and adds it to the originalData cluster
+	 * 
+	 * @param fileName
+	 * @throws FileNotFoundException
+	 */
+
+	public void readFile(String fileName) throws FileNotFoundException {
+		try {
+			Scanner fileReader = new Scanner(new File(fileName));
+			while (fileReader.hasNextLine()) {
+				String currentLine = fileReader.nextLine();
+				Scanner lineReader = new Scanner(currentLine);
+				double sampleCoords[] = new double[2];
+				sampleCoords[1] = lineReader.nextDouble();
+				sampleCoords[2] = lineReader.nextDouble();
+				Sample point = new Sample(sampleCoords);
+				this.originalData.add(point);
+
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+		}
+	}
+
 	public static void main(String[] args) {
 		// ask for the file name and the number of clusters
 		Scanner in = new Scanner(System.in);
